59af69104f9b61bf2c7f249fd280a14c834a40e0
John Baker <phibonacci85@gmail.com>
2018-04-11 16:23:02 -0500

    set up datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
+} from '@angular/material';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
 export class MaterialModule {}
