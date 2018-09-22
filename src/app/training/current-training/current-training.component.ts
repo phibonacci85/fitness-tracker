94b8fb78800b1b74d272cafff124923f8e6cbe94
John Baker <phibonacci85@gmail.com>
2018-09-22 02:06:08 -0500

    fixed small bug

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -14,7 +14,7 @@ import { take } from 'rxjs/operators';
 })
 export class CurrentTrainingComponent implements OnInit {
   progress = 0;
-  timer: number;
+  timer: any;
 
   constructor(
     private dialog: MatDialog,
